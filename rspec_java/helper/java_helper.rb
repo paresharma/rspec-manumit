d8ca430fc2229c1432f3cd8b31fbfef514ff2904
paresharma <paresh.brahm@gmail.com>
2015-10-11 21:01:42 +0200

    Hook capybara with poltergeist

---
+++ rspec_java/helper/java_helper.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+
+Capybara.default_driver = Capybara.javascript_driver = :poltergeist
+
+# Specify app host here
+# Capybara.app_host = 'http://localhost:9966'
+# Or read it from the environment
+Capybara.app_host = ENV['APP_HOST']
+
+# Add rest of the configuration below
