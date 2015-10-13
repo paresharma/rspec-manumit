96d40cf2b561915359531d192554d81d557b667e
paresh brahmacharimayum <paresh.brahm@gmail.com>
2015-10-13 23:42:34 +0200

    Switch off Capybara's rack server

--- lib/rspec_java/project_initializer/spec/java_helper.rb
+++ lib/rspec_java/project_initializer/spec/java_helper.rb
@@ -9,5 +9,6 @@ Capybara.default_driver = Capybara.javascript_driver = :poltergeist
 # Capybara.app_host = 'http://localhost:9966'
 # Or read it from the environment
 Capybara.app_host = ENV['APP_HOST']
+Capybara.run_server = false
 
 # Add rest of the configuration below
